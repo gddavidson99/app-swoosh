55ef3fae5e38852ff28f75d71c272e9569e4003b
Greg Davidson <gddavidson99@gmail.com>
2018-02-07 16:31:29 -0500

    Added segues to storyboards

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -19,12 +19,12 @@ class ViewController: UIViewController {
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+        
+        
     }
-
 
 }
 
